9fbc13f2aa1ac1711d01219f370ac4d795be4e95
Nathan Hood <nathanhood@me.com>
2017-08-15 01:35:24 -0500

    Add cart count

--- source/components/cart/index.vue
+++ source/components/cart/index.vue
@@ -4,6 +4,7 @@
 		<path d="M214.685 402.828c-24.83 0-45.03 20.2-45.03 45.03 0 24.828 20.2 45.028 45.03 45.028s45.03-20.2 45.03-45.03c-.002-24.828-20.202-45.028-45.03-45.028zm0 64.914c-10.966 0-19.887-8.922-19.887-19.887 0-10.966 8.922-19.887 19.887-19.887s19.887 8.922 19.887 19.887c0 10.967-8.922 19.887-19.887 19.887zM372.63 402.828c-24.83 0-45.03 20.2-45.03 45.03 0 24.828 20.2 45.028 45.03 45.028s45.03-20.2 45.03-45.03c-.002-24.828-20.202-45.028-45.03-45.028zm0 64.914c-10.966 0-19.887-8.922-19.887-19.887 0-10.966 8.922-19.887 19.887-19.887 10.966 0 19.887 8.922 19.887 19.887 0 10.967-8.922 19.887-19.887 19.887zM383.716 165.755h-180.15c-6.942 0-12.57 5.628-12.57 12.57 0 6.944 5.63 12.572 12.57 12.572h180.15c6.943 0 12.57-5.628 12.57-12.57 0-6.945-5.627-12.572-12.57-12.572zM373.91 231.035H213.374c-6.943 0-12.57 5.628-12.57 12.57s5.627 12.572 12.57 12.572H373.91c6.943 0 12.57-5.628 12.57-12.57 0-6.943-5.627-12.572-12.57-12.572z"/>
 		<path d="M506.34 109.744c-4.793-5.884-11.897-9.258-19.488-9.258H95.278l-7.908-38.39c-1.65-8.007-7.113-14.73-14.614-17.988L17.58 20.158c-6.37-2.767-13.774.156-16.537 6.524-2.766 6.37.157 13.774 6.524 16.537l55.178 23.95 60.826 295.26c2.397 11.63 12.753 20.07 24.626 20.07h301.166c6.943 0 12.57-5.63 12.57-12.572 0-6.943-5.627-12.57-12.57-12.57H148.197l-7.4-35.917H451.69c11.872 0 22.23-8.44 24.624-20.067l35.163-170.675c1.53-7.432-.34-15.07-5.136-20.954zM451.69 296.3H135.62l-35.162-170.673H486.85l-35.16 170.674z"/>
 	</svg>
+	<span class="cart__count" v-if="totalItems > 0">{{ totalItems }}</span>
 	<ul class="cart__list js-cart-list" :class="{ 'js-hide': isHidden }">
 		<li class="cart__item" v-for="(item, key) in items" v-if="items.length > 0">
 			<img :src="item.image.url" :alt="item.image.alt" class="cart__item-image">
@@ -44,20 +45,37 @@ export default {
 			items: []
 		}
 	},
+	computed: {
+		totalItems() {
+			return this.items.reduce((sum, item) => sum + item.quantity, 0);
+		}
+	},
 
 	watch: {
 		isHidden(value) {
+			const scope = this;
+
 			if (value === true) {
 				this.disableBodyEvent();
 			} else {
-				$events.on('body', 'click.cart', this.hideList);
+				$events.on({
+					body: {
+						click: this.hideList
+					},
+					window: {
+						scroll: this.hideList
+					}
+				}, {
+					namespace: 'cart'
+				});
 			}
 		}
 	},
 
 	methods: {
 		disableBodyEvent() {
-			$events.off('body', 'click.cart');
+			$events.off('body', 'body.cart');
+			$events.off('window', 'scroll.cart');
 		},
 		getProductUrl(itemSlug) {
 			return `${uri().origin}/products/${itemSlug}`;
@@ -77,21 +95,7 @@ export default {
 			this.isHidden = ! this.isHidden;
 		},
 		update(newItem) {
-			const itemsCount = this.items.length;
-			let itemExists = false;
-
-			for (let i = 0; i < itemsCount; i++) {
-				// Replace item if it exists
-				if (this.items[i].slug === newItem.slug) {
-					this.items[i] = newItem;
-					itemExists = true;
-					break;
-				}
-			}
-
-			if (! itemExists) {
-				this.items.push(newItem);
-			}
+			this.items.push(newItem);
 		}
 	}
 }
