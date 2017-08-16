adbdbaa039382fd76a4d07e2e4a937440fea94c3
Nathan Hood <nathanhood@me.com>
2017-08-16 15:33:37 -0500

    Update cart shown logic

--- source/components/cart/index.vue
+++ source/components/cart/index.vue
@@ -5,8 +5,8 @@
 		<path d="M506.34 109.744c-4.793-5.884-11.897-9.258-19.488-9.258H95.278l-7.908-38.39c-1.65-8.007-7.113-14.73-14.614-17.988L17.58 20.158c-6.37-2.767-13.774.156-16.537 6.524-2.766 6.37.157 13.774 6.524 16.537l55.178 23.95 60.826 295.26c2.397 11.63 12.753 20.07 24.626 20.07h301.166c6.943 0 12.57-5.63 12.57-12.572 0-6.943-5.627-12.57-12.57-12.57H148.197l-7.4-35.917H451.69c11.872 0 22.23-8.44 24.624-20.067l35.163-170.675c1.53-7.432-.34-15.07-5.136-20.954zM451.69 296.3H135.62l-35.162-170.673H486.85l-35.16 170.674z"/>
 	</svg>
 	<span class="cart__count" v-if="totalItems > 0">{{ totalItems }}</span>
-	<ul class="cart__list js-cart-list" :class="{ 'js-hide': isHidden }">
-		<li class="cart__item" v-for="(item, key) in items" v-if="items.length > 0">
+	<ul class="cart__list js-cart-list" v-show="shown">
+		<li class="cart__item" v-for="(item, key) in items" v-if="totalItems > 0">
 			<img :src="item.image.url" :alt="item.image.alt" class="cart__item-image">
 			<div class="cart__item-meta">
 				<a :href="getProductUrl(item.slug)" class="cart__item-name">{{ item.title }}</a>
@@ -14,7 +14,7 @@
 				<a href="#" class="cart__item-remove" @click.prevent.stop="removeItem(key)">Remove</a>
 			</div>
 		</li>
-		<li class="cart__empty" v-if="! items.length">
+		<li class="cart__empty" v-if="! totalItems">
 			The cart is empty
 		</li>
 	</ul>
@@ -30,22 +30,11 @@ import { cartStore } from './index.js';
 
 export default {
 	name: 'cart',
-	mounted() {
-		let data = cartStore.get('items');
-
-		if (data) {
-			this.items = data;
-		}
-
-		$mediator.on('cart.add', this.update);
-		$mediator.on('navigation', this.hideList);
-	},
 	data() {
 		return {
-			greeting: 'Hello',
-			isHidden: true,
+			shown: false,
 			items: []
-		}
+		};
 	},
 	computed: {
 		totalItems() {
@@ -54,8 +43,8 @@ export default {
 	},
 
 	watch: {
-		isHidden(value) {
-			if (value === true) {
+		shown(value) {
+			if (value === false) {
 				this.disableBodyEvent();
 			} else {
 				$events.on({
@@ -72,6 +61,17 @@ export default {
 		}
 	},
 
+	mounted() {
+		let data = cartStore.get('items');
+
+		if (data) {
+			this.items = data;
+		}
+
+		$mediator.on('cart.add', this.update);
+		$mediator.on('navigation', this.hideList);
+	},
+
 	methods: {
 		disableBodyEvent() {
 			$events.off('body', 'body.cart');
@@ -81,7 +81,7 @@ export default {
 			return `${uri().origin}/products/${itemSlug}`;
 		},
 		hideList() {
-			this.isHidden = true;
+			this.shown = false;
 		},
 		removeItem(index) {
 			this.items.splice(index, 1);
@@ -93,7 +93,7 @@ export default {
 				return;
 			}
 
-			this.isHidden = ! this.isHidden;
+			this.shown = ! this.shown;
 		},
 		update(newItem) {
 			this.items.push(newItem);
