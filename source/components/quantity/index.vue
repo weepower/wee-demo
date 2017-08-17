091f2a9cacf45382c3e3c3bf724102b69c9d6fed
Nathan Hood <nathanhood@me.com>
2017-08-17 15:55:24 -0500

    Refactor quantity component

--- source/components/quantity/index.vue
+++ source/components/quantity/index.vue
@@ -8,22 +8,22 @@ import dropdown from '../dropdown/index.vue';
 export default {
 	name: 'quantity',
 	components: { dropdown },
-	props: ['values'],
-	/**
-	 * Set the options for the dropdown
-	 */
-	created() {
-		for (let i = 0; i < this.values; i += 1) {
-			let value = i + 1;
-
-			this.options.push({ title: value, slug: value});
-		}
+	props: {
+		values: null
 	},
 	data() {
 		return {
 			options: [],
 			selected: 0
 		}
-	}
+	},
+	created() {
+		// Set the options for the dropdown
+		for (let i = 0; i < this.values; i += 1) {
+			let value = i + 1;
+
+			this.options.push({ title: value, slug: value});
+		}
+	},
 }
 </script>
